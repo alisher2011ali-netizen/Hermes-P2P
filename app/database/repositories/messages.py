fc44bc9abeb07b9553397109996fc408956b4d22
Алишер <alisher2011ali@gmail.com>
2026-04-20 11:42:23 +0300

    Update messages.py

--- app/database/repositories/messages.py
+++ app/database/repositories/messages.py
@@ -8,23 +8,11 @@ from app.database.models.secondary_models import Message
 
 async def save_message(
     session: AsyncSession,
-    contact_id: int,
-    encypted_content: bytes,
-    nonce: bytes,
-    signature_bytes: bytes,
-    timestamp: datetime,
-    is_outbox: bool,
+    new_msg: Message
 ):
     """Сохраняет зашифрованное сообщение."""
-    new_msg = Message(
-        contact_id=contact_id,
-        encrypted_content=encypted_content,
-        nonce=nonce,
-        signature_bytes=signature_bytes,
-        timestamp=timestamp,
-        is_outbox=is_outbox,
-    )
     session.add(new_msg)
+    await session.commit()
     return new_msg
 
 
